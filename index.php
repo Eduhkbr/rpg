5f0adacd12e1c7a126bc05d00a34ad118795754f
Eduardo Abreu <edu.abreu09@gmail.com>
2025-06-27 17:53:56 -0300

    feat(auth): Index e htaccess

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 
 spl_autoload_register(function ($class) {
     $prefix = 'App\\';
-    $base_dir = __DIR__ . '/../src/';
+    $base_dir = __DIR__ . './src/';
     $len = strlen($prefix);
     if (strncmp($prefix, $class, $len) !== 0) {
         return;
@@ -20,7 +20,7 @@ spl_autoload_register(function ($class) {
 
 // --- 2. Carregamento de Configuração e Conexão com o Banco ---
 
-$dbConfigFile = __DIR__ . '/../config/database.php';
+$dbConfigFile = __DIR__ . './config/database.php';
 
 if (!file_exists($dbConfigFile)) {
     die("<h1>Erro de Configuração</h1><p>Arquivo de configuração do banco de dados não encontrado. Por favor, copie <code>config/database.php.example</code> para <code>config/database.php</code> e preencha suas credenciais.</p>");
